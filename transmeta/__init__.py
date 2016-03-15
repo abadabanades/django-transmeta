bdfd138b571eba93afa8e2ebcc64b351c8dc8bb7
Daniel Abad <abadabanades@gmail.com>
2016-03-15 14:41:04 +0100

    Added translate_mandatory attribute for fields that need to be translated to all languages

--- transmeta/__init__.py
+++ transmeta/__init__.py
@@ -41,13 +41,18 @@ def get_real_fieldname_in_each_language(field):
 
 
 def canonical_fieldname(db_field):
-    """ all "description_en", "description_fr", etc. field names will return "description" """
-    return getattr(db_field, 'original_fieldname', db_field.name)  # original_fieldname is set by transmeta
+    """
+    All "description_en", "description_fr", etc. field names will
+    return "description"
+    original_fieldname is set by transmeta
+    """
+    return getattr(db_field, 'original_fieldname', db_field.name)
 
 
 def mandatory_language():
     """ returns mandatory language. the language which is required to fill """
-    return getattr(settings, 'TRANSMETA_MANDATORY_LANGUAGE', fallback_language())
+    return getattr(settings, 'TRANSMETA_MANDATORY_LANGUAGE',
+                   fallback_language())
 
 
 def fallback_language():
@@ -56,14 +61,18 @@ def fallback_language():
                    settings.LANGUAGE_CODE)
 
 
-def get_all_translatable_fields(model, model_trans_fields=None, column_in_current_table=False):
-    """ returns all translatable fields in a model (including superclasses ones) """
+def get_all_translatable_fields(model, model_trans_fields=None,
+                                column_in_current_table=False):
+    """ returns all translatable fields in a model (including superclasses) """
     if model_trans_fields is None:
         model_trans_fields = set()
-    model_trans_fields.update(set(getattr(model._meta, 'translatable_fields', [])))
+    model_trans_fields.update(
+        set(getattr(model._meta, 'translatable_fields', [])))
     for parent in model.__bases__:
-        if getattr(parent, '_meta', None) and (not column_in_current_table or parent._meta.abstract):
-            get_all_translatable_fields(parent, model_trans_fields, column_in_current_table)
+        if getattr(parent, '_meta', None) and \
+                (not column_in_current_table or parent._meta.abstract):
+            get_all_translatable_fields(parent, model_trans_fields,
+                                        column_in_current_table)
     return tuple(model_trans_fields)
 
 
@@ -112,33 +121,45 @@ class TransMeta(models.base.ModelBase):
     or if null, the value in the default language.
     '''
 
+    def get_translatable(attrs, meta_field):
+        if 'Meta' in attrs and hasattr(attrs['Meta'], meta_field):
+            fields = getattr(attrs['Meta'], meta_field)
+            delattr(attrs['Meta'], meta_field)
+            if not isinstance(fields, tuple):
+                raise ImproperlyConfigured(
+                    "Meta's translate attribute must be a tuple")
+            return fields, True
+        return (), False
+
+    def check_field_in_model(field, attrs, name):
+        if field not in attrs or \
+                not isinstance(attrs[field], models.fields.Field):
+            raise ImproperlyConfigured(
+                "There is no field %(field)s in model %(name)s, "
+                "as specified in Meta's translate attribute" %
+                dict(field=field, name=name))
+
     def __new__(cls, name, bases, attrs):
         attrs = OrderedDict(attrs)
-        if 'Meta' in attrs and hasattr(attrs['Meta'], 'translate'):
-            fields = attrs['Meta'].translate
-            delattr(attrs['Meta'], 'translate')
-        else:
+        fields, trans_1 = cls.get_translatable(attrs, 'translate')
+        mandatory, trans_2 = cls.get_translatable(attrs, 'translate_mandatory')
+
+        if not (trans_1 or trans_2):
             new_class = super(TransMeta, cls).__new__(cls, name, bases, attrs)
-            # we inherits possible translatable_fields from superclasses
-            abstract_model_bases = [base for base in bases if hasattr(base, '_meta') \
-                                    and base._meta.abstract]
+            # we inherit possible translatable_fields from superclasses
+            abstract_model_bases = [base for base in bases
+                                    if hasattr(base, '_meta') and
+                                    base._meta.abstract]
             translatable_fields = []
             for base in abstract_model_bases:
                 if hasattr(base._meta, 'translatable_fields'):
-                    translatable_fields.extend(list(base._meta.translatable_fields))
+                    translatable_fields.extend(
+                        list(base._meta.translatable_fields))
             new_class._meta.translatable_fields = tuple(translatable_fields)
             return new_class
 
-        if not isinstance(fields, tuple):
-            raise ImproperlyConfigured("Meta's translate attribute must be a tuple")
-
-        for field in fields:
-            if not field in attrs or \
-               not isinstance(attrs[field], models.fields.Field):
-                    raise ImproperlyConfigured(
-                        "There is no field %(field)s in model %(name)s, "\
-                        "as specified in Meta's translate attribute" % \
-                        dict(field=field, name=name))
+        for field in fields + mandatory:
+            cls.check_field_in_model(field, attrs, name)
             original_attr = attrs[field]
             for lang in get_languages():
                 lang_code = lang[LANGUAGE_CODE]
@@ -147,20 +168,23 @@ class TransMeta(models.base.ModelBase):
                 lang_attr.original_fieldname = field
                 lang_attr_name = get_real_fieldname(field, lang_code)
                 if lang_code != mandatory_language():
-                    # only will be required for mandatory language
-                    if not lang_attr.null and lang_attr.default is NOT_PROVIDED:
-                        lang_attr.null = True
-                    if not lang_attr.blank:
-                        lang_attr.blank = True
+                    if field in fields:
+                        # only will be required for mandatory language
+                        if not lang_attr.null and \
+                                lang_attr.default is NOT_PROVIDED:
+                            lang_attr.null = True
+                        if not lang_attr.blank:
+                            lang_attr.blank = True
                 if hasattr(lang_attr, 'verbose_name'):
-                    lang_attr.verbose_name = LazyString(lang_attr.verbose_name, _(lang_name))
+                    lang_attr.verbose_name = LazyString(lang_attr.verbose_name,
+                                                        _(lang_name))
                 attrs[lang_attr_name] = lang_attr
             del attrs[field]
             attrs[field] = property(default_value(field))
 
         new_class = super(TransMeta, cls).__new__(cls, name, bases, attrs)
         if hasattr(new_class, '_meta'):
-            new_class._meta.translatable_fields = fields
+            new_class._meta.translatable_fields = mandatory + fields
         return new_class
 
 
@@ -174,4 +198,4 @@ class LazyString(object):
         return u'%s (%s)' % (self.proxy, self.lang)
 
     def __str__(self):
-        return u'%s (%s)' % (self.proxy, self.lang)
+        return self.__unicode__()
